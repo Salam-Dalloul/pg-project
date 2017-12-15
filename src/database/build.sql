d3d75d4554ad30fab4edbb7571f95ab5d999e710
salam-dalloul <salamdalloul93@gmail.com>
2017-12-15 15:19:24 +0200

    sec

--- src/database/build.sql
+++ src/database/build.sql
@@ -1,15 +1,42 @@
 BEGIN;
 
-DROP TABLE IF EXISTS usersinformations CASCADE;
+DROP TABLE IF EXISTS books CASCADE;
 
-CREATE TABLE usersinformations (
+CREATE TABLE books (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  location VARCHAR(100) NOT NULL
+  title VARCHAR(100) NOT NULL,
+  author VARCHAR(100) NOT NULL
 );
 
-INSERT INTO usersinformations (name, location) VALUES
-('salam', 'gaza'),
-('shahy', 'gaza');
+INSERT INTO books (title, author) VALUES
+('Harry Potter and the Philosopher\'s Stone', 'J. K. Rowling'),
+('The Trial', 'Kafka'),
+('Twilight', 'Stephenie Meyer');
+
+DROP TABLE IF EXISTS visitors CASCADE;
+
+CREATE TABLE visitors (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+);
+
+INSERT INTO visitors (name) VALUES
+('salam'),
+('marwa'),
+('hanan');
+
+DROP TABLE IF EXISTS books_reservation CASCADE;
+
+CREATE TABLE books_reservation (
+  book_id integer REFERENCES books,
+  visitor_id integer REFERENCES visitors,
+  reservation_date date NOT NULL,
+  reservation_period integer NOT NULL,
+  PRIMARY KEY (book_id, visitor_id)
+);
+
+INSERT INTO books_reservation (book_id, visitor_id, reservation_date) VALUES
+(1, 1, '1/1/2018', 5),
+(2, 3, '4/1/2018', 3);
 
 COMMIT;
